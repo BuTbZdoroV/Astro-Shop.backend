7b30aa9c238066cac576c0ee87d923c0485b8188
BuTbZdoroV <andreydjevala@gmail.com>
2025-07-07 22:36:53 +0300

    fd

--- gateway-service/src/main/java/org/gatewayservice/security/config/WebClientConfig.java
+++ gateway-service/src/main/java/org/gatewayservice/security/config/WebClientConfig.java
@@ -1,15 +1,20 @@
 package org.gatewayservice.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${backend.url}")
+    public String backendURL;
+
     @Bean
     public WebClient authServiceWebClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8080")
+                .baseUrl(backendURL)
                 .build();
     }
 }
